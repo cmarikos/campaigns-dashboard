77070e542f5e218b3fe1f106b8bf21fa82152310
Christina Marikos <christina.marikos@cta-tech.app>
2025-08-13 23:30:38 +0000

    add doors, work on deep canvass queries

---
+++ deep_canvass_results.sql
@@ -0,0 +1,30 @@
+
+SELECT
+cw.pctnum
+--, CAST(c.DateCanvassed AS DATE) AS DateCanvassed
+, COUNT(c.VanID) AS ContactCount
+, COUNT(DISTINCT c.VanID) AS UniqueContactCount
+, COUNT(vb.vb_voterbase_household_id) AS DoorCount
+, COUNT(DISTINCT vb.vb_voterbase_household_id) AS UniqueDoorCount
+FROM `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsContacts_VF` AS c
+
+LEFT JOIN `prod-organize-arizon-4e1c0a83.targetsmart_AZ.voter_base_latest` as vb
+  ON c.VanID = vb.vb_smartvan_id
+
+-- dictionary table for contactypeid to contact type name since the VAN pipeline is... lacking
+LEFT JOIN `prod-organize-arizon-4e1c0a83.viewers_dataset.contacttypeid` AS t
+  ON c.ContactTypeID = t.ContactTypeID
+
+-- targetsmart precinct id to pctnum crosswalk so I can map canvassing in looker, details in az_precincts repo on how to make this
+INNER JOIN `prod-organize-arizon-4e1c0a83.rich_christina_proj.targetsmart_pctnum_crosswalk` AS cw
+  ON vb.vb_vf_national_precinct_code = cw.vb_vf_national_precinct_code
+
+LEFT JOIN `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsNotes_VF` AS n
+  ON c.VanID = n.VanID
+
+WHERE CAST(c.DateCanvassed AS DATE) > '2025-01-01'
+  AND c.CanvassedBy IS NOT NULL
+  AND ContactTypeName = 'Walk'
+  AND (cw.pctnum LIKE 'YU%' OR cw.pctnum LIKE 'PM%')
+
+GROUP BY 1
