743c56dfb195fcc5ac94d699b4aa32bc7f1590a7
Christina Marikos <christina.marikos@cta-tech.app>
2025-08-21 22:58:42 +0000

    doors, attempts, contacts with wkt geom for deep canvass

--- deep_canvass_results.sql
+++ deep_canvass_results.sql
@@ -1,11 +1,10 @@
+-- updated 8/21/2025
 
-SELECT
+CREATE OR REPLACE VIEW `prod-organize-arizon-4e1c0a83.viewers_dataset.campaigns_deep_canvass` AS(
+WITH doors AS(
+SELECT 
 cw.pctnum
---, CAST(c.DateCanvassed AS DATE) AS DateCanvassed
-, COUNT(c.VanID) AS ContactCount
-, COUNT(DISTINCT c.VanID) AS UniqueContactCount
-, COUNT(vb.vb_voterbase_household_id) AS DoorCount
-, COUNT(DISTINCT vb.vb_voterbase_household_id) AS UniqueDoorCount
+, COUNT(DISTINCT vb.vb_voterbase_household_id||CAST(c.DateCanvassed AS DATE)) AS doors
 FROM `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsContacts_VF` AS c
 
 LEFT JOIN `prod-organize-arizon-4e1c0a83.targetsmart_AZ.voter_base_latest` as vb
@@ -19,12 +18,85 @@ LEFT JOIN `prod-organize-arizon-4e1c0a83.viewers_dataset.contacttypeid` AS t
 INNER JOIN `prod-organize-arizon-4e1c0a83.rich_christina_proj.targetsmart_pctnum_crosswalk` AS cw
   ON vb.vb_vf_national_precinct_code = cw.vb_vf_national_precinct_code
 
-LEFT JOIN `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsNotes_VF` AS n
-  ON c.VanID = n.VanID
-
-WHERE CAST(c.DateCanvassed AS DATE) > '2025-01-01'
+  WHERE CAST(c.DateCanvassed AS DATE) > '2025-01-01'
   AND c.CanvassedBy IS NOT NULL
-  AND ContactTypeName = 'Walk'
-  AND (cw.pctnum LIKE 'YU%' OR cw.pctnum LIKE 'PM%')
+  AND t.ContactTypeName = 'Walk'
+  AND (cw.pctnum LIKE 'YU%' OR cw.pctnum LIKE 'PM%' OR cw.pctnum LIKE 'CH%')
 
-GROUP BY 1
+  GROUP BY 1
+),
+
+attempts AS(
+SELECT 
+cw.pctnum
+, COUNT(c.VanID) AS attempts
+FROM `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsContacts_VF` AS c
+
+LEFT JOIN `prod-organize-arizon-4e1c0a83.targetsmart_AZ.voter_base_latest` as vb
+  ON c.VanID = vb.vb_smartvan_id
+
+-- dictionary table for contactypeid to contact type name since the VAN pipeline is... lacking
+LEFT JOIN `prod-organize-arizon-4e1c0a83.viewers_dataset.contacttypeid` AS t
+  ON c.ContactTypeID = t.ContactTypeID
+
+-- targetsmart precinct id to pctnum crosswalk so I can map canvassing in looker, details in az_precincts repo on how to make this
+INNER JOIN `prod-organize-arizon-4e1c0a83.rich_christina_proj.targetsmart_pctnum_crosswalk` AS cw
+  ON vb.vb_vf_national_precinct_code = cw.vb_vf_national_precinct_code
+
+  WHERE CAST(c.DateCanvassed AS DATE) > '2025-01-01'
+  AND c.CanvassedBy IS NOT NULL
+  AND t.ContactTypeName = 'Walk'
+  AND (cw.pctnum LIKE 'YU%' OR cw.pctnum LIKE 'PM%' OR cw.pctnum LIKE 'CH%')
+
+  GROUP BY 1
+),
+
+contacts AS(
+SELECT 
+cw.pctnum
+,  COUNT(c.VanID) AS contacts
+FROM `prod-organize-arizon-4e1c0a83.raze_ngpvan_data.TSM_OneAZ_ContactsContacts_VF` AS c
+
+LEFT JOIN `prod-organize-arizon-4e1c0a83.targetsmart_AZ.voter_base_latest` as vb
+  ON c.VanID = vb.vb_smartvan_id
+
+-- dictionary table for contactypeid to contact type name since the VAN pipeline is... lacking
+LEFT JOIN `prod-organize-arizon-4e1c0a83.viewers_dataset.contacttypeid` AS t
+  ON c.ContactTypeID = t.ContactTypeID
+
+-- ditto above comment for this table
+LEFT JOIN `prod-organize-arizon-4e1c0a83.viewers_dataset.resultid` AS r 
+  ON c.ResultID = r.ResultID
+
+-- targetsmart precinct id to pctnum crosswalk so I can map canvassing in looker, details in az_precincts repo on how to make this
+INNER JOIN `prod-organize-arizon-4e1c0a83.rich_christina_proj.targetsmart_pctnum_crosswalk` AS cw
+  ON vb.vb_vf_national_precinct_code = cw.vb_vf_national_precinct_code
+
+  WHERE CAST(c.DateCanvassed AS DATE) > '2025-01-01'
+  AND c.CanvassedBy IS NOT NULL
+  AND t.ContactTypeName = 'Walk'
+  AND r.ResultShortName = 'Canvassed'
+  AND (cw.pctnum LIKE 'YU%' OR cw.pctnum LIKE 'PM%' OR cw.pctnum LIKE 'CH%')
+
+  GROUP BY 1
+)
+
+SELECT 
+pg.GEOMETRY
+, pg.PCTNUM 
+, pg.COUNTY
+, a.attempts
+, d.doors
+, c.contacts
+FROM `prod-organize-arizon-4e1c0a83.geofiles.az_precincts_geo` AS pg
+
+INNER JOIN attempts AS a
+ ON pg.PCTNUM = a.pctnum
+
+INNER JOIN doors AS d
+  ON pg.PCTNUM = d.pctnum
+
+INNER JOIN contacts AS c
+  ON pg.PCTNUM = c.pctnum
+
+)
